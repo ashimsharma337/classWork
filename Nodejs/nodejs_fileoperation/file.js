38efdb61d75b7ad1ae599c6cc9e45b5f345698ed
Ashim Sharma <ashimsharma337@gmail.com>
2021-12-08 14:02:29 +0545

    using callback for file operation

--- Nodejs/nodejs_fileoperation/file.js
+++ Nodejs/nodejs_fileoperation/file.js
@@ -1,36 +1,62 @@
-//import * as fileOp from "fs";
-const fileOp = require("fs");
+import * as fileOp from "fs";
+//const fileOp = require("fs");
 
+//using promise
 
-function writeInfile(file_name, data) {
+// function writeInfile(file_name, data) {
+//     return new Promise((resolve, reject) => {
+//         fileOp.writeFile("files/"+file_name, data, function(err) {
+//             if(err) {
+//                 reject(err);
+//             } else {
+//                 resolve("Success: File Write");
+//             }
+//        });
+//     });
+// }
+
+// using callbacks
+
+function writeInfile(file_name, data, cb) {
     return new Promise((resolve, reject) => {
-        fileOp.writeFile("files/"+file_name, data, function(err) {
-            if(err) {
-                reject(err);
-            } else {
-                resolve("Success: File Write");
-            }
-       });
+                 fileOp.writeFile("files/"+file_name, data, function(err) {
+                 if(err) {
+                    cb(err);  //callback function has two param, err or succ
+                 } else {
+                    cb(null,"Success: File Write");
+                 }
+         });
     });
 }
 
-
-
 function readFromfile(file_name) {
-    return Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         fileOp.readFile("files/"+file_name, {encoding:'utf8'}, function(err, done) {
             if(done) {
-                resolve("Done: ", done);
+                resolve(done);
             } else {
-               reject("Error: ", err);
+               reject(err);
             }
         });
     });
 }
 
+// using callback
+
+writeInfile("abc.txt", "From callback", function(err, done) {
+    if(err) {
+        console.log("Error: ", err);
+    } else {
+        console.log("Done: ", done);
+    }
+})
+
 // Exporting file
 // In ES5
-module.exports = {           //in es5 we need to pass an object, we can also assign key like a
-  a: writeInfile,
-  readFromfile
-};
+// module.exports = {           //in es5 we need to pass an object, we can also assign key like a
+//   a: writeInfile,
+//   readFromfile
+// };
+
+// Exporting file in es6
+// export {writeInfile, readFromfile} // we can not write a so while calling call function
--- Nodejs/nodejs_fileoperation/fileop.js
+++ Nodejs/nodejs_fileoperation/fileop.js
@@ -1,7 +1,25 @@
-//import { writeInfile, readFromfile } from "./file.js";
+import { writeInfile, readFromfile } from "./file.js";
 //importing using es5
-const fileOp = require("./file.js");
-console.log(fileOp);
+//const fileOp = require("./file.js");
+//console.log(fileOp);
 
-fileOp.a("def.txt", "string to write")
+// to write using es6
+writeInfile("def.txt", "string to write")
 .then((succ) => console.log(succ));
+
+// to read using es6
+readFromfile("def.txt")
+.then((data) => {
+    console.log("Data: " + data);
+})
+
+// to write using es5
+// fileOp.a("def.txt", "string to write")
+// .then((succ) => console.log(succ));
+
+// to read using es5
+// fileOp.readFromfile("def.txt")
+// .then((data) => {
+//     console.log("Data: " + data);
+// })
+
