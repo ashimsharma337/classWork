c1e0941d4a45d1f81a8211d0440b69b87f45a265
Ashim Sharma <ashimsharma337@gmail.com>
2021-12-29 07:58:52 +0545

    class added for cleaner code

---
+++ backend_app/new_api/controller/product_controller.js
@@ -0,0 +1,82 @@
+const ProductModel = require("../model/product_model");
+
+
+class ProductController {
+      
+getAllproducts(req, res, next) {
+    ProductModel.find(
+        {},
+        {},
+        function(err, product) {
+            if(err) {
+                next(err)
+            } else {
+            res.json(product);
+            }
+        })
+}
+
+addProduct(req, res, next) {
+    const product = new ProductModel(req.body);
+    product.save(function(err, success) {
+        if(err) {
+            next(err);
+        } else {
+            res.json(success);
+        }
+    });
+}
+
+getProductById(req, res, next) {
+    ProductModel.findOne({
+        _id: req.params.id
+         }, 
+        {},
+         function(err, product) {
+             if(err) {
+                 next(err)
+             } else {
+               res.json(product);
+             }
+             
+         });
+}
+
+updateProductById(req, res, next) {
+    ProductModel.updateOne({
+        _id: req.params.id
+         }, 
+        {
+            $set: req.body
+        },
+        {
+         upsert: true
+        },
+         function(err, success) {
+             if(err) {
+                 next(err)
+             } else {
+             res.json(success);
+             }
+         });
+}
+
+deleteProductById(req, res, next) {
+    ProductModel.deleteOne({
+        _id: req.params.id
+         }, 
+        {},
+         function(err, success) {
+             if(err) {
+                 next(err)
+             } else {
+             res.json(success);
+             }
+         });
+}
+}
+
+
+
+
+module.exports = ProductController;
