a57504da71a38b8ea3d0a2ac1aa8fba233a83a6f
Ashim Sharma <ashimsharma337@gmail.com>
2022-01-07 15:42:28 +0545

    State component added

--- React-front-end/myfrontend/src/index.js
+++ React-front-end/myfrontend/src/index.js
@@ -2,11 +2,13 @@ import react from "react";
 import reactdom from "react-dom";
 import App from "./components/login_components/Login"
 import Props from "./Props";
+import State from "./State";
 
 reactdom.render(
     <>
     <App/>
     <Props name="Ashim" address="kathmandu"></Props>
+    <State/>
     </>
     ,document.getElementById("app")
 );
