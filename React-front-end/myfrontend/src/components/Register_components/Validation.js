f29abd28ef6854675011de225df83bc3bfedcae0
Ashim Sharma <ashimsharma337@gmail.com>
2022-01-13 12:07:16 +0545

    form validation completed

--- React-front-end/myfrontend/src/components/Register_components/Validation.js
+++ React-front-end/myfrontend/src/components/Register_components/Validation.js
@@ -19,8 +19,8 @@ class Validation extends react.Component{
             },
             error: {
                 ...commonFields
-            }
-           
+            },
+            isValidForm: true
         }
     }
     handleChange = (e) => {
@@ -28,7 +28,7 @@ class Validation extends react.Component{
         // console.log(e.target.value);
         const {name, value} = e.target;
 
-        console.log("Before state: ", this.state);
+        // console.log("Before state: ", this.state);
 
         this.setState((preState) => ({
 
@@ -40,14 +40,54 @@ class Validation extends react.Component{
 
             }
         }), () => {
-            console.log("After set:", this.state);
+            //console.log("After set:", this.state);
+            this.validateForm(name);
         });
         // console.log(this.state);
     }
     handleSubmit = (e) => {
         e.preventDefault();
+        for (let key in commonFields) {
+            this.validateForm(key);
+        }
         console.log(this.state);
         // http client request
+    }
+    validateForm = fieldName => {
+        //validation logic
+        const {data} = this.state;
+        let errMsg = null;
+
+        switch(fieldName){
+            case "name":
+                errMsg = data.name != null ? '' : "Name is required!";
+                break;
+            case "email":
+                // http request for unique email
+                errMsg = data.email != null ? (data.email.includes('@') && data.email.includes('.com') ? '' : "Invalid email format") : "Email is required";
+                break;
+            case "password":
+                errMsg = data.password != null ? ((data.password.length >= 8) ? '' : "Password must be 8 digit") : "Password is required!";
+                break;
+            case "re_password":
+            errMsg = data.re_password != null ? ((data.password == data.re_password) ? '' : "Password and confirm Password must match") : "Re-Password  is required!";
+            break;
+            case "roles":
+                errMsg = data.roles != null ? '' : "Role is required!";
+                break;
+            default: 
+                break;
+        }
+
+        this.setState((preError) => ({
+             error: {
+                 ...preError.error,
+                 [fieldName]: errMsg
+             },
+             isValidForm: (errMsg ? true : false)
+        }), () => {
+            console.log("All Errors: ", this.state);
+        })
     }
     render(){
         return (<div className="container">
@@ -58,12 +98,14 @@ class Validation extends react.Component{
                         <label className ="col-sm-2 col-form-label">Name:</label>
                         <div className ="col-sm-10">
                         <input type="name" className="form-control" onChange={this.handleChange} name="name"/>
+                        <span className="text-danger">{this.state.error.name}</span>
                         </div>
                     </div>
                     <div className ="mb-3 mt-4 row">
                         <label className ="col-sm-2 col-form-label">Email:</label>
                         <div className ="col-sm-10">
                         <input type="email" className="form-control" onChange={this.handleChange} name="email"/>
+                        <span className="text-danger">{this.state.error.email}</span>
                         </div>
                     </div>
                     <div className ="mb-3 mt-4 row">
@@ -73,23 +115,26 @@ class Validation extends react.Component{
                                 <option value="customer">Customer</option>
                                 <option value="seller">Seller</option>
                             </select>
+                            <span className="text-danger">{this.state.error.roles}</span>
                         </div>
                     </div>
                     <div className ="mb-3 mt-4 row">
                         <label className ="col-sm-2 col-form-label">Password:</label>
                         <div className ="col-sm-10">
                         <input type="password" className="form-control" onChange={this.handleChange} name="password"/>
+                        <span className="text-danger">{this.state.error.password}</span>
                         </div>
                     </div>
                     <div className ="mb-3 mt-4 row">
                         <label className ="col-sm-2 col-form-label">Re-Password:</label>
                         <div className ="col-sm-10">
                         <input type="password" className="form-control" onChange={this.handleChange} name="re_password"/>
+                        <span className="text-danger">{this.state.error.re_password}</span>
                         </div>
                     </div>
                     <div className="row mb-3">
                               <div className="offset-md-2 col-sm-12 col-md-9">
-                                  <button className="btn btn-sm btn-success" onClick={this.handleSubmit} type="submit">Register</button>
+                                  <button disabled={this.state.isValidForm} className="btn btn-sm btn-success" onClick={this.handleSubmit} type="submit">Register</button>
                               </div> 
                     </div>
                   </form>
