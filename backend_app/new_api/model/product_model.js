0c3457f9352be89dd49a44b7aeeb0a65d244bd3a
Ashim Sharma <ashimsharma337@gmail.com>
2022-02-01 13:01:58 +0545

    product model updated

--- backend_app/new_api/model/product_model.js
+++ backend_app/new_api/model/product_model.js
@@ -1,20 +1,43 @@
 const mongoose = require("mongoose");
 
+const ReviewSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    rate: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5
+    }
+}, {
+    timestamps: true
+})
+
 const ProductSchema = new mongoose.Schema({
       title: {
           type: String,
           required: true,
           unique: true
       },
-      category: {
+      cat_id: {
           type: mongoose.Types.ObjectId,
           ref: "Category",
           default: null
       },
+      sub_cat_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "Category",
+        default: null
+      },
       price: {
           type: Number,
-          min: 50,
-          max: 200000
+          min: 1
       },
       discount: {
           type: Number,
@@ -22,9 +45,18 @@ const ProductSchema = new mongoose.Schema({
           max: 100,
           default: 0
       },
-      image: String,
       description: String,
-      manufacture_date: Date,
+      image: [String],
+      status: {
+        type: String,
+        enum: ['inactive', 'active'],
+        default: 'inactive'
+    },
+    is_featured: {
+        type: Boolean,
+        default: false
+    },
+    review: [ReviewSchema]
 }, {
     timestamps: true
 });
