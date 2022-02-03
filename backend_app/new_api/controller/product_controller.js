a93978c5f30f7384feed69e36561e369cdaf58b4
Ashim Sharma <ashimsharma337@gmail.com>
2022-02-03 20:40:05 +0545

    code added for product listing

--- backend_app/new_api/controller/product_controller.js
+++ backend_app/new_api/controller/product_controller.js
@@ -4,24 +4,39 @@ const ProductModel = require("../model/product_model");
 class ProductController {
       
 getAllproducts(req, res, next) {
-    ProductModel.find(
-        {},
-        {},
-        function(err, product) {
-            if(err) {
-                next(err)
-            } else {
-            res.json(product);
-            }
+    ProductModel.find()
+    .populate("cat_id")
+    .populate("sub_cat_id")
+    .then((products) => {
+        res.json({
+            result: products,
+            status: 200,
+            msg: "List success"
         })
+    })
+    .catch((error) => {
+        res.json({
+            result: (JSON.parse(error)),
+            status: 400,
+            msg: "Error while fetching data."
+        })
+    })
 }
 
 addProduct(req, res, next) {
     // console.log("req body", req.body);
     // console.log("req file", req.file);
-    if(req.file) {
-        req.body.image = req.file.filename;
+    if(req.files) {
+        // array
+        let image = [];
+        image = req.files.map((o) => o.filename);
+        // console.log("Image: ", image);
+        req.body.image = image;
+        // req.body.image = req.file.filename;
     }
+
+    // console.log(req.files);
+
     // if multiple image then.....req.files.map(object => object.filename)
     // console.log(req.body.image);
     const product = new ProductModel(req.body);
@@ -31,9 +46,17 @@ addProduct(req, res, next) {
     
     product.save(function(err, success) {
         if(err) {
-            next(err);
+            res.json({
+                result: null,
+                status:  400,
+                msg: JSON.stringify(err)
+            })
         } else {
-            res.json(success);
+            res.json({
+                result: product,
+                status:  200,
+                msg: "Product created successfully."
+            });
         }
     });
 }
