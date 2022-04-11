2b7eff03d821ff2a51298d7e7197f70b0bc3c2be
Ashim sharma <ashimsharma337@gmail.com>
2022-04-11 12:30:08 -0400

    Update app.js

--- Express_middleware/app.js
+++ Express_middleware/app.js
@@ -59,6 +59,21 @@ app.get("/profile", (req, res) => {
 
 app.use("/", router);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // response error page
+  res.status(err.status || 500);
+  res.send(err);
+});
 
 app.listen(8000, () => {
     console.log("server is listening at port 8000....");
